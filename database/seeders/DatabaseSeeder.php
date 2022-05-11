45da6d2d201be77fa7916b5c6c09e252a1eb3848
Alvaro Flores <almaflomon@gmail.com>
2022-05-11 15:58:03 -0400

    composer.lock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
-        // \App\Models\User::factory(10)->create();
-=======
         $this->call(RoleSeeder::class);
->>>>>>> 2600262892ef6b14191c23fd1b341d16364fc084
     }
 }
