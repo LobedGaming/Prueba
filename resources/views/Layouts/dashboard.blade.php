4dfe03021fb56f78bb243a927857765c730d9d6e
LobedGaming <andradeortizobed@gmail.com>
2022-05-30 15:02:12 -0600

    Arreglo de Salir

--- resources/views/Layouts/dashboard.blade.php
+++ resources/views/Layouts/dashboard.blade.php
@@ -139,11 +139,11 @@
                 <circle cx="12" cy="7" r="4" />
                 <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
             </svg></a>
-            <a href="">Salir <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="36" height="36" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <a href="{{route('logout')}}">Salir <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="36" height="36" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                   <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
                   <path d="M7 12h14l-3 -3m0 6l3 -3" />
-                  {{Auth::user()->logout}}
+                 
             </svg></a>
          </div>
         </nav>
