389512ad0180c192f7320426f7a50c9d645a41da
Your Name <you@example.com>
2022-06-01 18:13:53 -0400

    nada

--- resources/views/Layouts/dashboard.blade.php
+++ resources/views/Layouts/dashboard.blade.php
@@ -143,10 +143,7 @@
                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                   <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
                   <path d="M7 12h14l-3 -3m0 6l3 -3" />
-<<<<<<< HEAD
-=======
                  
->>>>>>> 4dfe03021fb56f78bb243a927857765c730d9d6e
             </svg></a>
          </div>
         </nav>
