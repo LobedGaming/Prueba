69070aba7d4b5cf6917fa70ff37ab16791effec5
LobedGaming <andradeortizobed@gmail.com>
2022-06-03 15:11:57 -0600

    cambios de obed historico

--- resources/views/Historico/show.blade.php
+++ resources/views/Historico/show.blade.php
@@ -15,37 +15,7 @@
                 <td> <span>{{$cita->paciente->user->name}}</span></td>
                 <td>  <span>{{$cita->fecha_hora}}</span></td>
                 <td>  <span>{{$cita->description}}</span></td>
-                <td> <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Receta</button>
-                  @foreach($recetas as $receta)
-                  @if($receta->cita_id==$cita->id )
-                    
-                  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="exampleModalLabel">Nueva Receta</h5>
-                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-          
-                        <div class="modal-body">
-                          <form action="{{route('receta.store')}}" method="post">
-                            @csrf
-                            <div class="mb-3">
-                              <label for="inputOtros" class="form-label">Descripción</label>
-                              <textarea class="form-control" style="resize: none;" id="inputOtros" name="description" id="" cols="30" rows="15" placeholder="Ingrese una descripción">{{$receta->description}}</textarea>
-                            </div>
-                          </form>
-                        </div>
-          
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                        </div>
-                        
-                      </div>
-                    </div>
-                  </div>
-                  @endif
-                  @endforeach
+                <td>  <a href="{{route('receta.show', $cita->id)}}" class="btn btn-info">Ver Receta</a></td>
               </div>
             </td>
               </tr>
--- resources/views/Receta/show.blade.php
+++ resources/views/Receta/show.blade.php
@@ -19,34 +19,27 @@
                         <p>{{$doctor[0]->name}}</p>
                         <p>Lic. en {{$doctor[1]->especialidad}}</p>
                     </div>
-
                     <div class="end"></div>
-                    
                 </div>
-
                 <div class="card-body" style="background-color: rgb(241, 241, 239);">
                     <div class="d-flex justify-content-between">
                         <div class="d-flex">
                             <p  class="fw-bold fs-5">Paciente:</p>
                             <p class="ms-2 fs-5">{{$patient->name}}</p>
                         </div>
-
                         <div class="d-flex">
                             <p  class="fw-bold fs-5">Fecha:</p>
                             <p class="ms-2 fs-5">{{$receta->fecha_hora}}</p>
                         </div>
                     </div>
-
                     <div class="">
                         <p class="fw-bold fs-5">Descripción</p>
                         <p>{{$receta->description}}</p>
                     </div>
-
                     <div class="firm d-flex justify-content-end">
                         <p class="fw-bold fs-5 me-2" style="">Firma</p>
                         <div style="background-color: rgb(241, 241, 239); width: 15%; height: 30px; border:0; border-bottom: 2px solid black;"></div>
                     </div>
-
                 </div>
             </div>
             <br> <br> <br>
