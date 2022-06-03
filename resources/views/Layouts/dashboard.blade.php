716106476d888888543b015f086331364f28da04
Alvaro Flores <almaflomon@gmail.com>
2022-06-03 16:43:48 -0400

    Citas, Historico admin y Recetas terminado

--- resources/views/Layouts/dashboard.blade.php
+++ resources/views/Layouts/dashboard.blade.php
@@ -139,7 +139,7 @@
     <div class="container-right">
         
         <nav class="d-flex justify-content-between">
-            <a href="">DashBoard</a>
+            <a href="/home">DashBoard</a>
             <div>
             <a href="">{{Auth::user()->name}}<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
